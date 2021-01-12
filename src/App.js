f617bc5d1f56a80d6506fba6603177839935bfeb
ldandoy <ldandoy@gmail.com>
2021-01-12 17:41:11 +0100

    MAJ

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import AccueilScreen from "./AccueilScreen"
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {senarios: []};
-  }
-
-  componentDidMount() {
-    fetch('https://mjvirtuelapi.herokuapp.com/api/senarios')
-    .then(response => response.json())
-    .then((senarios) => {
-      console.log(senarios);
-      this.setState({
-        'senarios': senarios
-      });
-    })
-    .catch((error) => {
-      console.error(error)
-    })
   }
 
   render() {
-    const items = this.state.senarios.map((senario) =>
-        <li key={senario.id}>{senario.title}</li>
-    );
-
     return <div className="App">
-      <h1>Bienvenue sur le Maite du Jeu virtuel.</h1>
-      <h2>Choisis le sénario que tu veux faire avec tes amis:</h2>
-      <ul>
-        {items}
-      </ul>
+      <BrowserRouter>
+      <Navigation />
+        <Route path="/" exact={true} component={AccueilScreen} />
+    </BrowserRouter>
     </div>
   }
 }
